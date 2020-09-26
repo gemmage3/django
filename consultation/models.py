8b9e868093a9e09cd906bca783ef09a6dc90d85f
gemma gonputh <gemmage3@hotmail.co.uk>
2020-09-26 16:30:09 +0100

    Home.html

--- consultation/models.py
+++ consultation/models.py
@@ -5,9 +5,6 @@ from django.db import models
 class Doctor(models.Model):
   first_name = models.CharField(max_length=30)
   last_name = models.CharField(max_length=30)
-  address = models.CharField(max_length=150, blank=True, null=True)
-  virtual_consultation = models.BooleanField()
-  years_experience = models.IntegerField()
-  languages = models.CharField(max_length=30, blank=True, null=True)
-  price_quote = models.BooleanField()
+  address = models.TextField(default = "Testing")
+
 
