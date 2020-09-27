75ab43d54b42e585a08fd3be4debf56fc802a416
gemma gonputh <gemmage3@hotmail.co.uk>
2020-09-27 15:55:53 +0100

    Doctor Model

--- consultation/models.py
+++ consultation/models.py
@@ -1,10 +1,36 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
 class Doctor(models.Model):
+  ENGLISH = 0
+  SPANISH = 1
+  FRENCH = 2
+  GERMAN = 3
+  CHINESE = 4
+  LANGUAGE_CHOICES = (
+    (0, 'English'),
+    (1, 'Spanish'),
+    (2, 'French'),
+    (3, 'German'),
+    (4, 'Chinese')
+    )
+
   first_name = models.CharField(max_length=30)
   last_name = models.CharField(max_length=30)
   address = models.TextField(default = "Testing")
+  county = models.ForeignKey('Country', on_delete=models.CASCADE, default="Testing")
+  virtual_consultation = models.BooleanField(default=False)
+  languages = models.PositiveIntegerField(choices = LANGUAGE_CHOICES,blank=True, default="0"
+    )
+  rating = models.PositiveIntegerField(validators=[MinValueValidator(0), MaxValueValidator(10)], default="0"
+    )
+  price_quote_available = models.BooleanField(default=False)
+
+
+class Country(models.Model):
+  country_name = models.CharField(max_length=30)
+
 
 
--- pages/views.py
+++ pages/views.py
@@ -2,5 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def home_view(*args, **kwargs):
-    return render(request, "home.html", {})
+def home_view(request, *args, **kwargs):
+    my_context = {
+      "my_text": "This is my home"
+    }
+    return render(request, "home.html", my_context)
